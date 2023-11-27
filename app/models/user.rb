afe57f84f9b7f34e92a6aaea9882337849adbc85
Joao Mateus <jaomateus@gmail.com>
2023-11-27 22:40:02 +0000

    add user first and last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
 
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-      # Add additional fields as needed
-    end
-  end
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 end
