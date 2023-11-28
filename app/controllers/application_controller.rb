ba46ca50429a51b811302e52af39e095bb506601
Joao Mateus <jaomateus@gmail.com>
2023-11-28 21:42:08 +0000

    finish user info card

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name user_photo])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name user_photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name user_photo profession])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name user_photo profession])
   end
 
 end
