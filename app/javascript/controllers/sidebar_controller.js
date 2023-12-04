fdf819b060e070e7be3bf2d0b1a1e2d41088b63c
Joao Mateus <jaomateus@gmail.com>
2023-12-04 19:50:25 +0000

    add right sliding menu

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -2,10 +2,15 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="sidebar"
 export default class extends Controller {
-  static targets = ["slideLeft", "slideRight", "changeIcon"];
+  static targets = [
+    "slideLeft",
+    "slideRight",
+    "changeIconRight",
+    "changeIconLeft",
+  ];
 
   slideLeft() {
-    const icon = this.changeIconTarget;
+    const icon = this.changeIconLeftTarget;
 
     this.slideLeftTarget.classList.toggle("sidebar-left-hidden");
     if (icon.src.includes("sidebar_left_collapse_icon")) {
@@ -16,6 +21,13 @@ export default class extends Controller {
   }
 
   slideRight() {
-    this.slideRightTarget.classList.toggle("d-none");
+    const icon = this.changeIconRightTarget;
+
+    this.slideRightTarget.classList.toggle("sidebar-right-hidden");
+    if (icon.src.includes("sidebar_right_collapse_icon")) {
+      icon.src = "/assets/icons/sidebar_left_collapse_icon.svg";
+    } else {
+      icon.src = "/assets/icons/sidebar_right_collapse_icon.svg";
+    }
   }
 }
