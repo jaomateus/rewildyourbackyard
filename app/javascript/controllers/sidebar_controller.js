4fa5cf515c04383028c535a506cd6f5e8d4a7346
Joao Mateus <jaomateus@gmail.com>
2023-12-04 23:10:38 +0000

    finish sidebar left slide functionality

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -1,33 +1,22 @@
 import { Controller } from "@hotwired/stimulus";
 
-// Connects to data-controller="sidebar"
 export default class extends Controller {
-  static targets = [
-    "slideLeft",
-    "slideRight",
-    "changeIconRight",
-    "changeIconLeft",
-  ];
+  static targets = ["sidebarTools", "changeIconLeft", "dashboardMain"];
 
-  slideLeft() {
+  connect() {
+    console.log("fsdfsdf");
+  }
+
+  toggle_sidebar_left() {
     const icon = this.changeIconLeftTarget;
 
-    this.slideLeftTarget.classList.toggle("sidebar-left-hidden");
+    this.sidebarToolsTarget.classList.toggle("sidebar-tools-visible");
+    this.dashboardMainTarget.classList.toggle("expanded");
+
     if (icon.src.includes("sidebar_left_collapse_icon")) {
       icon.src = "/assets/icons/sidebar_right_collapse_icon.svg";
     } else {
       icon.src = "/assets/icons/sidebar_left_collapse_icon.svg";
-    }
-  }
-
-  slideRight() {
-    const icon = this.changeIconRightTarget;
-
-    this.slideRightTarget.classList.toggle("sidebar-right-hidden");
-    if (icon.src.includes("sidebar_right_collapse_icon")) {
-      icon.src = "/assets/icons/sidebar_left_collapse_icon.svg";
-    } else {
-      icon.src = "/assets/icons/sidebar_right_collapse_icon.svg";
     }
   }
 }
